dc1f732b8f81a20c045fe772afa22c990fcc0be5
Ali <ali@gmail.com>
2023-01-17 14:44:08 +0100

    Bring the project back to life

--- mysql_nodejs_api/controllers/circuit.controller.js
+++ mysql_nodejs_api/controllers/circuit.controller.js
@@ -1,49 +1,49 @@
 'use strict';
 const Circuit = require('../models/circuit.model');
-exports.findAll = function(req, res) {
-Circuit.findAll(function(err, circuit) {
-  console.log('controller')
-  if (err)
-  res.send(err);
-  console.log('res', circuit);
-  res.send(circuit);
-});
+exports.findAll = function (req, res) {
+  Circuit.findAll(function (err, circuit) {
+    console.log('controller')
+    if (err)
+      res.send(err);
+    console.log('res', circuit);
+    res.send(circuit);
+  });
 };
-exports.create = function(req, res) {
-const new_circuit = new Circuit(req.body);
-//handles null error
-if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-  res.status(400).send({ error:true, message: 'Please provide all required field' });
-}else{
-Circuit.create(new_circuit, function(err, circuit) {
-  if (err)
-  res.send(err);
-  res.json({error:false,message:"Circuit added successfully!",data:circuit});
-});
-}
+exports.create = function (req, res) {
+  const new_circuit = new Circuit(req.body);
+  //handles null error
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).send({ error: true, message: 'Please provide all required field' });
+  } else {
+    Circuit.create(new_circuit, function (err, circuit) {
+      if (err)
+        res.send(err);
+      res.json({ error: false, message: "Circuit added successfully!", data: circuit });
+    });
+  }
 };
-exports.findById = function(req, res) {
-Circuit.findById(req.params.id, function(err, circuit) {
-  if (err)
-  res.send(err);
-  res.json(circuit);
-});
+exports.findById = function (req, res) {
+  Circuit.findById(req.params.id, function (err, circuit) {
+    if (err)
+      res.send(err);
+    res.json(circuit);
+  });
 };
-exports.update = function(req, res) {
-  if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-    res.status(400).send({ error:true, message: 'Please provide all required field' });
-  }else{
-    Circuit.update(req.params.id, new Circuit(req.body), function(err, circuit) {
-   if (err)
-   res.send(err);
-   res.json({ error:false, message: 'Circuit successfully updated' });
-});
-}
+exports.update = function (req, res) {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).send({ error: true, message: 'Please provide all required field' });
+  } else {
+    Circuit.update(req.params.id, new Circuit(req.body), function (err, circuit) {
+      if (err)
+        res.send(err);
+      res.json({ error: false, message: 'Circuit successfully updated' });
+    });
+  }
 };
-exports.delete = function(req, res) {
-Circuit.delete( req.params.id, function(err, circuit) {
-  if (err)
-  res.send(err);
-  res.json({ error:false, message: 'Circuit successfully deleted' });
-});
+exports.delete = function (req, res) {
+  Circuit.delete(req.params.id, function (err, circuit) {
+    if (err)
+      res.send(err);
+    res.json({ error: false, message: 'Circuit successfully deleted' });
+  });
 };
--- mysql_nodejs_api/models/circuit.model.js
+++ mysql_nodejs_api/models/circuit.model.js
@@ -1,70 +1,70 @@
 'use strict';
 var dBcon = require('../config/db.config');
 //Circuit object create
-var Circuit = function(circuit){
-  this.CircuitID        = circuit.CircuitID;
-  this.CircuitRef       = circuit.CircuitRef;
-  this.CircuitName      = circuit.CircuitName;
-  this.Location         = circuit.Location;
-  this.Country          = circuit.Country;
-  this.lat              = circuit.lat;
-  this.lng              = circuit.lng;
+var Circuit = function (circuit) {
+  this.CircuitID = circuit.CircuitID;
+  this.CircuitRef = circuit.CircuitRef;
+  this.CircuitName = circuit.CircuitName;
+  this.Location = circuit.Location;
+  this.Country = circuit.Country;
+  this.lat = circuit.lat;
+  this.lng = circuit.lng;
 
 };
 Circuit.create = function (newCircuit, result) {
-dBcon.query("INSERT INTO circuits set ?", newCircuit, function (err, res) {
-if(err) {
-  console.log("error: ", err);
-  result(err, null);
-}
-else{
-  console.log(res.insertId);
-  result(null, res.insertId);
-}
-});
+  dBcon.query("INSERT INTO circuits set ?", newCircuit, function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    }
+    else {
+      console.log(res.insertId);
+      result(null, res.insertId);
+    }
+  });
 };
 Circuit.findById = function (id, result) {
-dBcon.query("Select * from circuits where CircuitID = ? ", id, function (err, res) {
-if(err) {
-  console.log("error: ", err);
-  result(err, null);
-}
-else{
-  result(null, res);
-}
-});
+  dBcon.query("Select * from circuits where CircuitID = ? ", id, function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+    }
+    else {
+      result(null, res);
+    }
+  });
 };
 Circuit.findAll = function (result) {
-dBcon.query("Select * from circuits", function (err, res) {
-if(err) {
-  console.log("error: ", err);
-  result(null, err);
-}
-else{
-  console.log('circuits : ', res);
-  result(null, res);
-}
-});
+  dBcon.query("Select * from circuits", function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+    }
+    else {
+      console.log('circuits : ', res);
+      result(null, res);
+    }
+  });
 };
-Circuit.update = function(id, circuit, result){
-dBcon.query("UPDATE circuits SET CircuitID=?,CircuitRef=?,CircuitName=?,Location=?,Country=?,lat=?,lng=? WHERE id = ?", [circuit.CircuitID,circuit.CircuitRef,circuit.CircuitName,circuit.Location,circuit.Country,circuit.lat,circuit.lng, id], function (err, res) {
-if(err) {
-  console.log("error: ", err);
-  result(null, err);
-}else{
-  result(null, res);
-}
-});
+Circuit.update = function (id, circuit, result) {
+  dBcon.query("UPDATE circuits SET CircuitID=?,CircuitRef=?,CircuitName=?,Location=?,Country=?,lat=?,lng=? WHERE id = ?", [circuit.CircuitID, circuit.CircuitRef, circuit.CircuitName, circuit.Location, circuit.Country, circuit.lat, circuit.lng, id], function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+    } else {
+      result(null, res);
+    }
+  });
 };
-Circuit.delete = function(id, result){
-dBcon.query("DELETE FROM circuits WHERE CircuitID = ?", [id], function (err, res) {
-if(err) {
-  console.log("error: ", err);
-  result(null, err);
-}
-else{
-  result(null, res);
-}
-});
+Circuit.delete = function (id, result) {
+  dBcon.query("DELETE FROM circuits WHERE CircuitID = ?", [id], function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+    }
+    else {
+      result(null, res);
+    }
+  });
 };
-module.exports= Circuit;
+module.exports = Circuit;
